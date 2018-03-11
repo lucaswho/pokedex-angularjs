4faa82c6752693f4347ca8d2ca0365b153718de6
Lucas Chagas <lucasdchagas@yahoo.com.br>
2018-03-11 03:43:53 -0300

    calling the poke api to populate the grid (part 1)

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -9,8 +9,30 @@ angular.module('myApp.home', ['ngRoute'])
     });
   }])
 
-  .controller('homeController', ['$window', function ($window) {
+  .controller('homeController', ['$http', function ($http) {
 
     var vm = this;
+    vm.pokemonArray = [];
+
+    getPokemon();
+
+    function getPokemon() {
+
+      $http.get("https://pokeapi.co/api/v2/pokemon/?limit=12&offset=0")
+        .then(function (response) {
+          var data = response.data;
+          vm.pokemonArray = data.results;
+          angular.forEach(vm.pokemonArray, function (value, key) {
+            value.id = (key+1).pad(3);
+            value.imageUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + value.id + '.png';
+          });
+        });
+    }
 
   }]);
+
+Number.prototype.pad = function (size) {
+  var s = String(this);
+  while (s.length < (size || 2)) { s = "0" + s; }
+  return s;
+}
